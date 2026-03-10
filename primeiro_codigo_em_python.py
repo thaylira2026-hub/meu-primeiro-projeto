1532e264cd9589db7a6452e0c6e567b9593d80a7
Thays Lira <thay.lira2026@gmail.com>
2026-03-10 13:22:18 -0300

    Meu primeiro commit

--- primeiro_codigo_em_python.py
+++ primeiro_codigo_em_python.py
@@ -1,5 +1,8 @@
+
 time_casa = input("Time da casa: ")
 gols_casa = input("Gols do time da casa: ")
+
 time_visitante = input("Time visitante: ")
-gols_visitante = inpout("Gols do time visitante: ")
-print(time_visitante, " ", gols_visitante, "X", time_casa, " ", gols_casa)
+gols_visitante = input("Gols do time visitante: ")
+
+print(time_visitante, " ", gols_visitante, "X", time_casa, " ",gols_casa)
